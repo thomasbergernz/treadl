75b4d1217350a77e55c9cd1a2d705bceb60fab19
Will Webberley <me@wilw.dev>
2022-04-15 08:26:33 +0000

    Add Sentry integration for tracking errors

--- web/src/api/uploads.js
+++
@@ -1,9 +0,0 @@
-import api from 'api';
-
-export const uploads = {
-  generateFileUploadRequest({
-    forType, forId, name, size, type,
-  }, success, fail) {
-    api.authenticatedRequest('GET', `/uploads/file/request?name=${name}&size=${size}&type=${type}&forType=${forType}&forId=${forId}`, null, success, fail);
-  },
-};
