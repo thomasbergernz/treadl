6d9a1edc5c914435209b7c3da36d21d5df3eede2
Will Webberley <me@wilw.dev>
2022-05-08 16:30:41 +0000

    Public pages using env vars correctly

--- web/src/components/docs/index.js
+++ web/src/components/docs/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container, Divider, Grid, Card, Message, Button, Icon } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams} from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import utils from 'utils/utils.js';
 
 function Docs() {
+  const { doc } = useParams();
   return (
     <Container style={{marginTop: 40}}>
       <h1>{utils.appName()} documentation</h1>
+      <p>{utils.appName()} is built on the open-source Treadl software, which is documented on these pages.</p>
       <Divider section />
 
       <Grid stackable>
@@ -27,7 +29,9 @@ function Docs() {
             content={<p>That's great! Please reach out to us by emailing <a href={`mailTo:${process.env.REACT_APP_CONTACT_EMAIL}`}>{process.env.REACT_APP_CONTACT_EMAIL}</a>.</p>}
           />
 
-          <Button style={{position: 'fixed', bottom: 40}} onClick={() => window.scrollTo(0, 0)} secondary><Icon name='arrow up' /> Back to the top</Button>
+          {doc &&
+            <Button style={{position: 'fixed', bottom: 40}} onClick={() => window.scrollTo(0, 0)} secondary><Icon name='arrow up' /> Back to the top</Button>
+          }
         </Grid.Column>
 
         <Grid.Column width={12}>
