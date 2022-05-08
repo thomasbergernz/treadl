9fcd193a8722a75c181a731436610c64fa3656a2
Will Webberley <me@wilw.dev>
2022-05-08 08:49:51 +0000

    Functional component for usersearch

--- web/src/components/includes/UserSearch.js
+++ web/src/components/includes/UserSearch.js
@@ -1,58 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Menu, Popup, Input } from 'semantic-ui-react';
-import { connect } from 'react-redux'
 import api from 'api';
 
-class UserSearch extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = { open: false, searchTerm: '', oldSearchTerm: '', searchResults: null, searching: false };
-  }
-  componentDidMount() {
-    this.searcher = setInterval(() => {
-      if (this.state.searchTerm !== this.state.oldSearchTerm) this.search();
-    }, 500);
-  }
-  componentWillUnmount() {
-    clearInterval(this.searcher);
-  }
+function UserSearch({ onSelected, fluid }) {
+  const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [oldSearchTerm, setOldSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState();
+  const [searching, setSearching] = useState(false);
 
-  updateSearchTerm = e => {
-    this.setState({ searching: true, searchTerm: e.target.value })
-  }
-  search = () => {
-    this.setState({ open: false, searching: true, oldSearchTerm: this.state.searchTerm });
-    if (!this.state.searchTerm) return this.setState({ searching: false });
-    api.search.users(this.state.searchTerm, searchResults => this.setState({ open: true, searching: false, searchResults }), err => this.setState({searching: false}));
-  }
-  
-  inputClicked = () => {
-    if (this.state.searchResults) this.setState({ open: true });
-  }
-  onSelected = user => {
-    this.props.onSelected && this.props.onSelected(user);
-  }
+  const updateSearchTerm = e => {
+    setSearching(true);
+    setSearchTerm(e.target.value);
+  };
 
-  render() {
-    const { searchResults, searching, open } = this.state;
-    const { fluid } = this.props;
-    return (
-      <Popup hoverable position='bottom left' open={open} onClose={e => this.setState({ open: false })}
-        trigger={<Input fluid={fluid} icon='search' iconPosition='left' placeholder='Search for a username...' onChange={this.updateSearchTerm} loading={searching} onClick={this.inputClicked}/>}
-        content={(
-          <Menu borderless vertical>
-            {searchResults && searchResults.map(r => 
-              <Menu.Item key={r._id} as='a' icon='user' content={r.username} onClick={e => this.onSelected(r)} image={r.avatarUrl}/>
-            )}
-          </Menu>
-        )}
-      />
-    );
-  }
+  const search = useCallback(() => {
+    if (searchTerm === oldSearchTerm) return;
+    setOpen(false);
+    setSearching(true);
+    setOldSearchTerm(searchTerm);
+    if (!searchTerm) return setSearching(false);
+    api.search.users(searchTerm, searchResults => {
+      setOpen(true);
+      setSearching(false);
+      setSearchResults(searchResults);
+    }, () => setSearching(false));
+  }, [oldSearchTerm, searchTerm]);
+  useEffect(() => {
+    const searcher = setInterval(() => search(), 500);
+    return () => clearInterval(searcher);
+  }, [search]);
+
+  const inputClicked = () => {
+    if (searchResults) setOpen(true);
+  };
+  const choose = user => {
+    onSelected && onSelected(user);
+  };
+
+  return (
+    <Popup hoverable position='bottom left' open={open} onClose={e => setOpen(false)}
+      trigger={<Input fluid={fluid} icon='search' iconPosition='left' placeholder='Search for a username...' onChange={updateSearchTerm} loading={searching} onClick={inputClicked}/>}
+      content={(
+        <Menu borderless vertical>
+          {searchResults && searchResults.map(r =>
+            <Menu.Item key={r._id} as='a' icon='user' content={r.username} onClick={e => choose(r)} image={r.avatarUrl}/>
+          )}
+        </Menu>
+      )}
+    />
+  );
 }
 
-
-const UserSearchContainer = connect(null, null)(UserSearch);
-
-export default UserSearchContainer;
+export default UserSearch;
