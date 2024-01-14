2a55cfcc2e68c0eb3eac06d181a3d973300a00f2
Will Webberley <me@wilw.dev>
2024-01-14 23:05:26 +0000

    remove unneeded files

--- mobile/lib/routeArguments.dart
+++
@@ -1,6 +0,0 @@
-class ProjectScreenArguments {
-  final String projectId;
-  final String projectName;
-  final String projectPath;
-  ProjectScreenArguments(this.projectId, this.projectName, this.projectPath);
-}
--- mobile/lib/store.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class Store extends ChangeNotifier {
-  String? apiToken;
-
-  void setToken(String? newToken) {
-    apiToken = newToken;
-  }
-}
